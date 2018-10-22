ef8889c3a3d3d4ff3edd03b8a12c40ce08f52283
Jehm09 <estephenher999@gmail.com>
2018-10-21 20:57:46 -0500

    Updated BST and fixed bugs

--- src/interfaces/IABB.java
+++ src/interfaces/IABB.java
@@ -1,9 +1,19 @@
 package interfaces;
 
+//import EstructurasDeDatos.NodeTree;
+
 public interface IABB <K extends Comparable<K>,V>{
 
-	public void addElementABB(K key, V value);
-	public void removeElementABB(K key);
-	public V searchElementABB(K key);
+	public void put(K key, V value);
+	public void delete(K key);
+	public V get(K key);
+	public void deleteMin();
+	public void deleteMax();
+	public K min();
+	public K max();
+//	public int size();
+	public int height();
+	public boolean isEmpty();
+//	public NodeTree<K, V> searchNode(K key);
 	
 }
