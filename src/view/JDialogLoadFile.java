e87879ce5337322ff4a1cd50d0c4d1db7fcebe71
Christian Andrew C <Christianandrew741@gmail.com>
2018-10-23 22:33:04 -0500

    Update classes

--- src/view/JDialogLoadFile.java
+++ src/view/JDialogLoadFile.java
@@ -20,38 +20,46 @@ public class JDialogLoadFile extends JDialog implements ActionListener {
 	public static final String ACEPT = "acept";
 	
 	public static final String LOAD = "load";
+	public static final String ADD_PLAYER = "add";
 	
 	private JLabel lbLoad;
 	private JButton btAcept;
 	private JButton btLoad;
+	private JButton btAddPlayer;
+	private JDialogAddPlayer jDialogAddPlayer;
 	
 	
 	public JDialogLoadFile() {
 		
-		
+		jDialogAddPlayer = new JDialogAddPlayer();
 		setLayout(null);
 		setVisible(false);
-		setSize(400, 180);
+		setSize(400, 230);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setTitle("Load");
 		
 		
-		lbLoad  = new JLabel("Select file");
+		lbLoad  = new JLabel("Add Player");
 		btAcept = new JButton("Acept");
 		btAcept.addActionListener(this);
 		btAcept.setActionCommand(ACEPT);
-		btLoad = new JButton("Load");
+		btLoad = new JButton("Load players");
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		btLoad.addActionListener(this);
 		btLoad.setActionCommand(LOAD);
+		btAddPlayer = new JButton("Add player");
+		btAddPlayer.addActionListener(this);
+		btAddPlayer.setActionCommand(ADD_PLAYER);
 		setLayout(null);
 		lbLoad.setBounds(176,10, 70,70);
 		btLoad.setBounds(158, 60, 100, 40);
 		btAcept.setBounds(158, 100, 100, 40);
+		btAddPlayer.setBounds(158, 140, 100, 40);
 		add(lbLoad);
 		add(btLoad);
 		add(btAcept);
+		add(btAddPlayer);
 		
 		
 		
@@ -75,6 +83,8 @@ public class JDialogLoadFile extends JDialog implements ActionListener {
 				loadFile();
 			}else if (comand.equals(ACEPT)) {
 				this.dispose();
+			}else if (comand.equals(ADD_PLAYER)) {
+				jDialogAddPlayer.setVisible(true);
 			}
 		
 	}
